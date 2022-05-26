27f08621591bfab0d743fdb7e23fa03cc90b2050
Md. Ifrat Hasnat <ifrathasnat@instawebworks.com.au>
2022-05-26 11:25:03 +0600

    verify-token

--- routes/authentication.js
+++ routes/authentication.js
@@ -65,45 +65,6 @@ router.post("/login", async (req, res) => {
     res.status(500).json(err)
   }
 });
-// router.post('/login', async (req, res) => {
-//   try{
-//       const user = await User.findOne(
-//           {
-//               userName: req.body.user_name
-//           }
-//       );
 
-//       !user && res.status(401).json("Wrong User Name");
-
-//       const hashedPassword = CryptoJS.AES.decrypt(
-//           user.password,
-//           process.env.PASS_SEC
-//       );
-
-
-//       const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-//       const inputPassword = req.body.password;
-      
-//       originalPassword != inputPassword && 
-//           res.status(401).json("Wrong Password");
-
-//       const accessToken = jwt.sign(
-//       {
-//           id: user._id,
-//           isAdmin: user.isAdmin,
-//       },
-//       process.env.JWT_SEC,
-//           {expiresIn:"3d"}
-//       );
-
-//       const { password, ...others } = user._doc;  
-//       res.status(200).json({...others, accessToken});
-
-//   }catch(err){
-//       res.status(500).json(err);
-//   }
-
-// });
 
 module.exports = router;
