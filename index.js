c9ba618c2bf87aaec8ce50be911539b7e5f524cb
Md. Ifrat Hasnat <ifrathasnat@instawebworks.com.au>
2022-06-01 13:09:29 +0600

    cart-api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/authentication");
 const productRoute = require("./routes/product");
 const userRoute = require("./routes/user");
+const cartRoute = require("./routes/cart");
 
 const cors = require("cors");
 
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use("/api/authentication", authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts" , cartRoute)
 
 
 app.listen(process.env.PORT || 5000, () => {
